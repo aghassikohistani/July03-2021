0e3eb7042b569f8705594cd0af7e37d31d3fc55e
aghassi kohistani <aghassikohistani@gmail.com>
2021-07-03 14:41:34 -0700

    new tea code

---
+++ src/eng/Match.java
@@ -0,0 +1,12 @@
+package eng;
+
+public class Match {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("England beat Ukrine 4 zero");
+
+	}
+
+}
---
+++ src/eng/Tea.java
@@ -0,0 +1,20 @@
+package eng;
+
+public class Tea {
+	
+	
+	
+	public Tea() {
+		
+		System.out.println("constructor");
+	}
+
+
+
+   public void drinkTea() {
+	   
+	   System.out.println("This is the new tea spot");
+   }
+
+
+}
